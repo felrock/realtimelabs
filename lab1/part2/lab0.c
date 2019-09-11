b49ea07d4d910514767d5aae3c9a4bfe9000f1d3
Felix <stenbackfelix@gmail.com>
2019-09-11 10:28:22 +0200

    changed wait and other things

--- lab1/part2/lab0.c
+++ lab1/part2/lab0.c
@@ -5,13 +5,20 @@
 */
 
 #include <rpi3.h>
+
 /* Function to wait */
 void wait(long ms)
 {
     /* Random value since we don't know the freq,
      * but it is not 1200Mhz or 300Mhz or anything we
-     * found online. */
-    long time_to_wait = 3000L * ms; 
+     * found online. 
+     *
+     * *edit* lets say its 1200Mhz and the for loop takes
+     * about 8 cycles per iteration, then the constant
+     * should be (1200000/7) to ms -> 171.42 cycles per
+     * ms wait.
+     * */
+    long time_to_wait = 172 * ms; 
     for(long i=0L; i<time_to_wait; i++)
     {
       asm("nop");
@@ -21,7 +28,7 @@ void wait(long ms)
 int main()
 {
     /* Enable GPIO16 as an output */
-    long delay=100;
+    long delay=1000;
     GPIO->GPFSEL1 |= (1 << 18);
 
     while(1) 
