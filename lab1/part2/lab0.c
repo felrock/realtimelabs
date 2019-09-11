45fb11b048ca574d6b080bdcfec74f4e829c401e
Felix <stenbackfelix@gmail.com>
2019-09-11 10:49:36 +0200

    fixed wait and removed stuff

--- lab1/part2/lab0.c
+++ lab1/part2/lab0.c
@@ -14,11 +14,14 @@ void wait(long ms)
      * found online. 
      *
      * *edit* lets say its 1200Mhz and the for loop takes
-     * about 8 cycles per iteration, then the constant
-     * should be (1200000/7) to ms -> 171.42 cycles per
+     * about 7 cycles per iteration, then the constant
+     * should be (1200000000/7) to ms -> 171428 cycles per
      * ms wait.
+     *
+     * *edit 2* throw logic out the window and trial and error.
+     * 
      * */
-    long time_to_wait = 172 * ms; 
+    long time_to_wait = 1722 * ms; 
     for(long i=0L; i<time_to_wait; i++)
     {
       asm("nop");
@@ -34,7 +37,7 @@ int main()
     while(1) 
     {
         GPIO->GPSET0 |= (1 << 16);
-        wait(delay);
+        wait(50);
         GPIO->GPCLR0 |= (1 << 16);
         wait(delay);
         delay *= 2;
