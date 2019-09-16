51ce5a529cf29449a2e3261ee6709df4e1af897d
Jonatan <jonajo15@student.hh.se>
2019-09-16 14:58:07 +0200

    nocommecnts

--- lab2/part1/main.c
+++ lab2/part1/main.c
@@ -3,8 +3,8 @@
 #include "expStruct.h"
 
 int main(){
-	ExpStruct v = *iexp(4);
-	printf("expInt: %d\n", v.expInt);
-	printf("expFraction: %d\n", v.expFraction);
+	ExpStruct * v = iexp(4);
+	printf("expInt: %d\n", v->expInt);
+	printf("expFraction: %d\n", v->expFraction);
 	return 0;
 }
