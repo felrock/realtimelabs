e100d71e833fc17f8cc1286c872136176507ebb9
Jonatan <jonajo15@student.hh.se>
2019-09-16 14:25:59 +0200

    minor change

--- lab2/part1/expStruct.c
+++ lab2/part1/expStruct.c
@@ -3,18 +3,18 @@
 #include "expStruct.h"
 
 
-// T(x) = 1 + x + x^2/2! + x^3/3!
+// T(x) = 1 + x + x^2/2! + x^3/3! ... + x^n/n!
 ExpStruct * iexp (int n){
 	ExpStruct exp;
 	float ans = 1;
 	float temp = 1;
 
-	for(int i = 1; i <= 20; i++){ // change 20 for different accuracy
-		temp = (temp*n)/i;
+	for(int i = 1; i <= n; i++){
+		temp = temp*n/i;
 		ans = ans + temp;
 	}
 
-	float dec = ans - ((int)ans);
+	float dec = ans - (int)ans;
 	exp.expInt = (int)ans;
 	exp.expFraction = (int)(dec*100);//100 gives 2 decimal precision
 	ExpStruct *reExp = &exp;
--- lab2/part1/main.c
+++ lab2/part1/main.c
@@ -3,7 +3,7 @@
 #include "expStruct.h"
 
 int main(){
-	ExpStruct v = *iexp(5);
+	ExpStruct v = *iexp(4);
 	printf("expInt: %d\n", v.expInt);
 	printf("expFraction: %d\n", v.expFraction);
 	return 0;
