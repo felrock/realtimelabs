190d7e0d5e883f0d43c2188c97ffa980fdc827b1
Jonatan <jonajo15@student.hh.se>
2019-09-18 10:56:37 +0200

    finished cunit + comments

--- lab2/part1/expStruct.h
+++ lab2/part1/expStruct.h
@@ -1,9 +1,10 @@
 
 /**
  *  expStruct
- *  An expStruct is a structure which represents 2 32-bit register and
+ *  An expStruct is a structure which represents 2 integers and
  *  is equipped with a operation to calculate the value of e^n
- *	(n = the input parameter) with two degrees of precision.
+ *	(n = the input parameter) with two degrees of precision. This is calculated
+ *	using taylor series with (n+1) terms.
  */
 
 typedef struct expStruct ExpStruct;
@@ -13,27 +14,27 @@ struct expStruct {
 	int expFraction;
 };
 
-/** @brief Calculates Exponential function of e^n
+/** @brief Calculates Exponential function of e^n using taylor series, and
+ *	returns a struct expStruct which holds 2 integers, the integer part and the
+ * 	fractional part. the function uses n+1 number of terms to calculate.
  *
- *  @param n, an Integer..
+ *  @param n, an Integer which holds the value of the exponent.
  *
- *  @return void
+ *  @return struct extStruct
  *
- *  Pre-condition: iRegister != Null
+ *  Pre-condition: 0 <= n <= 22
  *
- *  Post-condition: after reset(i, r) all bits of iRegister is 0
- *  properties:
- *  after resetAll(r),  getBit(i, r) = 0
- *  if getBit(i, r) == 0 then
- *    getBit(j, r) returns 0 for all j
- *  0 <= j < 32 and j <> i before and after resetBit(i, r)
+ *  Post-condition: after iexp(n) both integers are set.
+ *
+ *  properties: After iexp(n) the attributes can be expected accordingly if
+ *	pre-conditions are followed.
+ *	1 <= expInt < 1994556672
+ * 	0 <= expFraction <= 99
  *
  *  test-cases:
- *  Allocate memory to an iRegister r
- *  first set r->content to random value
- *  then do resetAll(&r),
- *  then all memory should be equal to 0, by using assert
- *  display the result after each and every call by
- *    printf("test x is passed!");
+ *	the function is tested with n as a value:
+ *	1. in the middle of the pre-condition
+ *	2. at the edges.
+ *	3. outside the pre-conditions.
  */
 ExpStruct * iexp (int n);
