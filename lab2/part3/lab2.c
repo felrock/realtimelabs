e70a69c635b9a7831da76f09167b62f5993a3cbe
Jonatan <jonajo15@student.hh.se>
2019-09-18 14:26:12 +0200

    removed comment

--- lab2/part3/lab2.c
+++ lab2/part3/lab2.c
@@ -12,18 +12,6 @@
 
 void wait(long ms)
 {
-    /* Random value since we don't know the freq,
-     * but it is not 1200Mhz or 300Mhz or anything we
-     * found online.
-     *
-     * *edit* lets say its 1200Mhz and the for loop takes
-     * about 7 cycles per iteration, then the constant
-     * should be (1200000000/7) to ms -> 171428 cycles per
-     * ms wait.
-     *
-     * *edit 2* throw logic out the window and trial and error.
-     *
-     * */
     long time_to_wait = 1722 * ms;
     for(long i=0L; i<time_to_wait; i++)
     {
